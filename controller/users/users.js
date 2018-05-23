c504284757fb3e97b996686c9c502efd5d4cafb3
Walker Wang <walker.wang@atck.io>
2018-05-23 16:31:53 +0800

    Config Data & Preset Data Page done Usage Detail => more Page done

--- controller/users/users.js
+++ controller/users/users.js
@@ -1,6 +1,13 @@
 const UserData = require('../../models/UserData');
 
 class Users {
+    constructor() {
+        this.database = null;
+        this.data = this.data.bind(this);  // 改变data函数中的this指向
+        this.detail = this.detail.bind(this);
+        this.Config = this.Config.bind(this);
+        this.more = this.more.bind(this);
+    }
     async users(req, res, next) {
         try {
             const doc = await UserData.find();
@@ -18,28 +25,40 @@ class Users {
         try {
             const doc = await UserData.findById(id);
             if (doc) {
+                this.database = doc;
                 res.render('data', {
                     data: doc,
                 });
             }
         } catch (e) {
-            console.log('err:' ,e);
+            console.log('error:',e);
         }
     }
 
     async detail(req, res, next) {
-        let id = req.params.id;
-        try {
-            const doc = await UserData.findById(id);
-            if (doc) {
-                res.render('detail', {
-                    data: doc,
-                });
-            }
-        }
-         catch (e) {
-            console.log('err:', e);
-        }
+        let index = req.params.index;
+        res.render('detail', {
+            data: this.database,
+            index: index,
+        })
+    }
+
+    async more(req, res, next) {
+        let i = req.params.i;
+        let index = req.params.index;
+        res.render('more', {
+            data: this.database,
+            index: index,
+            i: i,
+        })
+    }
+
+    async Config(req, res, next) {
+        let index = req.params.index;
+        res.render('ConfigPreset', {
+            data: this.database,
+            index: index,
+        });
     }
 }
 
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,8 @@ const Users = require('../controller/users/users');
 /* GET users listing. */
 router.get('/', Users.users);
 router.get('/data/:id', Users.data);
-router.get('/data/detail/:id', Users.detail);
+router.get('/data/detail/:index', Users.detail);
+router.get('/data/detail/more/:index/:i', Users.more);
+router.get('/data/ConfigPreset/:index', Users.Config);
 
 module.exports = router;
